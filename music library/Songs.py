77c80c81994e5bb01b4e2a6346d9a89b6509cab6
aminkhalaj73 <mohammad.a.khalaj@gmail.com>
2019-01-28 14:58:33 +0330

    music library modified

--- music library/Songs.py
+++ music library/Songs.py
@@ -80,6 +80,27 @@ class Artist:
                 """
                 self.albums.append(Album)
 
+        def Add_song(self , name , year , title):
+                """ Add a new song to the collection of albums
+                
+                This method will add song to an album in the collection.
+                A new album will be created in the collection if it doesn't already exist
+
+                Args:
+                        name (str) : The name of the song
+                        year (int) : The year the album was produced
+                        title (str) : The title of the song
+                """
+                album_found = find_object(name , self.albums)
+                if album_found is None :
+                        print(name + " not found")
+                        album_found = Album(name , year , self)
+                        self.Add_album(album_found)
+                else:
+                        print("Found album" + name)
+
+                album_found.add_song(title)
+
 def find_object(field , object_list):
         """Check 'ovject_list' to see if an object with a 'name' attribute equal to 'field' exists, return it if so."""
         for item in object_list:
@@ -89,8 +110,6 @@ def find_object(field , object_list):
 
 
 def load_data():
-        new_artist = None
-        new_album = None
         artist_list = []
 
         with open("albums.txt" , "r") as albums:
@@ -100,37 +119,13 @@ def load_data():
                         year_field =int(year_field)
                         print("{}:{}:{}:{}".format(artist_field,album_field,year_field,song_field))
 
+                        new_artist = find_object(artist_field , artist_list)
                         if new_artist is None :
                                 new_artist = Artist(artist_field)
                                 artist_list.append(new_artist)
-                        elif new_artist.name != artist_field :
-                                # We've just read details from new artist
-                                # Retrieve the artist object if there's one ,
-                                #Otherwise create new artist object and add it to the artist list.
-                                new_artist = find_object(artist_field, artist_list)
-                                if new_artist is None:
-                                        new_artist = Artist(artist_field)
-                                        artist_list.append(new_artist)
-                                new_album = None
 
-                        if new_album is None :
-                                new_album = Album(album_field , year_field , artist_field)
-                                new_artist.Add_album(new_album)
-                        elif new_album.name != album_field :
-                                # we've just read details from new album for the current artist
-                                # Retrieve Album object if there is one
-                                # Otherwise create new album object and store it in the Artist's collection
-                                new_album = find_object(album_field, new_artist.albums)
-                                if new_album is None:
-                                        new_album = Album(album_field , year_field , new_artist)
-                                        new_artist.Add_album(new_album)
+                        new_artist.Add_album(album_field, year_field , song_field)
 
-
-                        # create new song object and add it to the current album's collection
-                        new_song = Song(song_field , new_artist)
-                        new_album.add_song(new_song)
-                # After read last line of the text file , we will have an artist and album that haven't been stored - process them now
-        
         return artist_list
 
 def create_checkfile(artist_list):
