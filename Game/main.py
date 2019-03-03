7669d84a75d90afb665610e5a4f81f8e5c241e34
aminkhalaj73 <mohammad.a.khalaj@gmail.com>
2019-03-03 14:28:18 +0330

    modifications added

---
+++ Game/main.py
@@ -0,0 +1,27 @@
+
+from enemy import Enemy , Troll , Vampyre , vampyreking
+
+# random_monster = Enemy("basic enemy" , 12 , 1)
+# print(random_monster)
+
+# random_monster.take_damage(6)
+# print(random_monster)
+
+# ugly_troll = Troll("pug")
+# print("Ugly troll - {}".format(ugly_troll))
+# ugly_troll.take_damage(18)
+# print(ugly_troll)
+
+# vamp = Vampyre("Vlad")
+# print(vamp)
+# vamp.take_damage(5)
+# print(vamp)
+
+# while vamp._alive :
+#     vamp.take_damage(1)
+#     print(vamp)
+
+vampk = vampyreking("Niko")
+print(vampk)
+vampk.take_damage(16)
+print(vampk)
